bf99c252594cb6f054d7e133b4ea0a875015f326
Haris Yunanda Rangkuti <91673258+hayuraaa@users.noreply.github.com>
2024-02-22 23:22:06 +0700

    Update All Futures

--- app-4.py
+++ app-4.py
@@ -16,18 +16,19 @@ import plotly.graph_objects as go
 # Streamlit app
 def main():
     st.title("Prediksi Crypto Metaverse")
-    st.write("Prediksi Menggunakan Model Long Short Term Memory dan Gated Recurrent Unit")
-    
+    st.write("Sistem Prediksi menggunakan Algoritma Long Short Term Memory dan Gated Recurrent Unit, sistem ini menggunakan framework streamlit untuk web interaktif.")
     # Sidebar Input Data
+    #--------------------#
     st.sidebar.header("Data Download")
     stock_symbol =st.sidebar.selectbox("Masukkan Nama Coin:", ["STX4847-USD", "ICP-USD", "SAND-USD", "THETA-USD", "MANA-USD"])
     start_date = st.sidebar.date_input("Start Date", pd.to_datetime("2022-01-01"))
     end_date = st.sidebar.date_input("End Date", pd.to_datetime("2024-01-01"))
-
     # Download stock price data
     data = yf.download(stock_symbol, start=start_date, end=end_date)
+    #--------------------#
 
-    # Proses Data
+    # Proses Seluruh Data | 4 Future
+    #------------------------------#
     close_prices = data['Close'].values.reshape(-1, 1)
     scaler_close = MinMaxScaler(feature_range=(0, 1))
     scaled_data_close = scaler_close.fit_transform(close_prices)
@@ -35,41 +36,55 @@ def main():
     open_prices = data['Open'].values.reshape(-1, 1)
     scaler_open = MinMaxScaler(feature_range=(0, 1))
     scaled_data_open = scaler_open.fit_transform(open_prices)
+    
+    high_prices = data['High'].values.reshape(-1, 1)
+    scaler_high = MinMaxScaler(feature_range=(0, 1))
+    scaled_data_high = scaler_high.fit_transform(high_prices)
+    #------------------------------#
+
 
     # Data preparation
+    #-----------------#
     n_steps = 120
     X, y = prepare_data_close(scaled_data_close, n_steps)
     A, b = prepare_data_open(scaled_data_open, n_steps)
+    C, d = prepare_data_high(scaled_data_high, n_steps)
 
     # Splitting into train and test sets
     train_size_close = int(len(X) * 0.8)
     X_train, X_test = X[:train_size_close], X[train_size_close:]
     y_train, y_test = y[:train_size_close], y[train_size_close:]
     
-    # Splitting into train and test sets
     train_size_open = int(len(A) * 0.8)
     A_train, A_test = A[:train_size_open], A[train_size_open:]
     b_train, b_test = b[:train_size_open], b[train_size_open:]
     
-
+    train_size_high = int(len(C) * 0.8)
+    C_train, C_test = C[:train_size_high], C[train_size_high:]
+    d_train, d_test = d[:train_size_high], b[train_size_high:]
+    #-----------------#
+    
     # Reshape data for LSTM and GRU models
     X_train_lstm = X_train.reshape((X_train.shape[0], X_train.shape[1], 1))
     X_test_lstm = X_test.reshape((X_test.shape[0], X_test.shape[1], 1))
     X_train_gru = X_train.reshape((X_train.shape[0], X_train.shape[1], 1))
     X_test_gru = X_test.reshape((X_test.shape[0], X_test.shape[1], 1))
 
+
     # Sidebar for model selection
+    #---------------------------#
     st.sidebar.header("Select Model")
     model_type = st.sidebar.selectbox("Select Model Type:", ["LSTM", "GRU"])
-    
 
     # Mengambil Mode
     if model_type == "LSTM":
         final_model = load_model("stx_model_lstm.h5")
     else:
         final_model = load_model("stx_model_gru.h5")
+    #---------------------------#
 
     # Model evaluation
+    #-----------------#
     y_pred = final_model.predict(X_test)
     y_pred = scaler_close.inverse_transform(y_pred)
     y_test_orig = scaler_close.inverse_transform(y_test.reshape(-1, 1))
@@ -77,28 +92,39 @@ def main():
     b_pred = final_model.predict(A_test)
     b_pred = scaler_open.inverse_transform(b_pred)
     b_test_orig = scaler_open.inverse_transform(b_test.reshape(-1, 1))
+    
+    d_pred = final_model.predict(C_test)
+    d_pred = scaler_high.inverse_transform(d_pred)
+    d_test_orig = scaler_high.inverse_transform(d_test.reshape(-1, 1))
 
     # Perhitungan Evaluasi
+    #--------------------#
     mse_close = mean_squared_error(y_test_orig, y_pred)    #  perhitungan MSE
-    rmse_close = math.sqrt(mse_close)                            #  perhitungan RMSE    
+    rmse_close = math.sqrt(mse_close)                      #  perhitungan RMSE    
     mad_close = np.mean(np.abs(y_test_orig - y_pred))      #  perhitungan MAD
     mape_close = np.mean(np.abs((y_test_orig - y_pred) / y_test_orig)) * 100
     
     mse_open = mean_squared_error(b_test_orig, b_pred)    #  perhitungan MSE
-    rmse_open = math.sqrt(mse_open)                            #  perhitungan RMSE    
+    rmse_open = math.sqrt(mse_open)                       #  perhitungan RMSE    
     mad_open = np.mean(np.abs(b_test_orig - b_pred))      #  perhitungan MAD
     mape_open = np.mean(np.abs((b_test_orig - b_pred) / b_test_orig)) * 100
-
-    CloseTab, OpenTab, ActualTab, ResultTab = st.tabs(["Close", "Open", "Aqtual", "All Result"])
+    
+    mse_high = mean_squared_error(d_test_orig, d_pred)    #  perhitungan MSE
+    rmse_high = math.sqrt(mse_high)                       #  perhitungan RMSE    
+    mad_high = np.mean(np.abs(d_test_orig - d_pred))      #  perhitungan MAD
+    mape_high = np.mean(np.abs((d_test_orig - d_pred) / d_test_orig)) * 100
+    
+    
+    #-----------------Selected Price----------------#
+    CloseTab, OpenTab, HighTab, ActualTab, ResultTab = st.tabs(["Close", "Open", "High", "Aqtual", "All Result"])
     with CloseTab:
         # Display results
-        st.header(f"Results for {model_type} Model")
+        st.header(f"Results Close Price for {model_type} Model")
         st.write("Root Mean Squared Error (RMSE):", round(rmse_close, 5))
         st.write("Mean Absolute Deviation (MAD):", round (mad_close, 5))
         st.write("Mean Absolute Percentage Error (MAPE):", round (mape_close, 5))
         
         # Visualize predictions
-        st.header("Visual Prediksi")
         visualize_predictions_close(data, train_size_close, n_steps, y_test_orig, y_pred)
         
         # Display combined actual and predicted data table with time information
@@ -134,13 +160,12 @@ def main():
         
     with OpenTab:
         # Display results
-        st.header(f"Results for {model_type} Model")
+        st.header(f"Results Open Price for {model_type} Model")
         st.write("Root Mean Squared Error (RMSE):", round(rmse_open, 5))
         st.write("Mean Absolute Deviation (MAD):", round (mad_open, 5))
         st.write("Mean Absolute Percentage Error (MAPE):", round (mape_open, 5))
         
         # Visualize predictions
-        st.header("Visual Prediksi")
         visualize_predictions_open(data, train_size_open, n_steps, b_test_orig, b_pred)
         
         # Display combined actual and predicted data table with time information
@@ -160,10 +185,38 @@ def main():
         # Display the combined data table
         st.table(combined_data_open)
         
+    
+    with HighTab:
+        # Display results
+        st.header(f"Results High Price for {model_type} Model")
+        st.write("Root Mean Squared Error (RMSE):", round(rmse_high, 5))
+        st.write("Mean Absolute Deviation (MAD):", round (mad_high, 5))
+        st.write("Mean Absolute Percentage Error (MAPE):", round (mape_high, 5))
+        
+        # Visualize predictions
+        visualize_predictions_high(data, train_size_high, n_steps, d_test_orig, d_pred)
+        
+        # Display combined actual and predicted data table with time information
+        st.header("Table High Harga Asli dan Harga Prediksi")
+        st.write("Data range:", data.index[train_size_high + n_steps:].min(), "to", data.index[train_size_high + n_steps:].max())
+        price_difference_high = d_test_orig.flatten() - d_pred.flatten()
+        percentage_difference_high = (price_difference_high / d_test_orig.flatten()) * 100
+        predicted_prices_str_high = [f"{val:.5f}" for val in d_pred.flatten()]
+        combined_data_high = pd.DataFrame({
+            'Tanggal': data.index[train_size_high + n_steps:],
+            'Actual_Prices': d_test_orig.flatten(),
+            'Predicted_Prices': predicted_prices_str_high,
+            'Price_Difference': abs(price_difference_high),
+            'Percentage_Difference': abs(percentage_difference_high)
+        })
+        combined_data_high['Percentage_Difference'] = combined_data_high['Percentage_Difference'].map("{:.2f}%".format)
+        # Display the combined data table
+        st.table(combined_data_high)
+        
 
     with ActualTab:
         # Display results
-        st.header(f"Results for {model_type} Model")
+        st.header(f"Results Actual Price for {model_type} Model")
         
         # Plot all prices
         fig_all_prices = go.Figure()
@@ -195,13 +248,12 @@ def main():
         
     with ResultTab:
         # Display results
-        st.header(f"Results for {model_type} Model")
+        st.header(f"Results All Price Prediction for {model_type} Model")
         # Visualize predictions
-        st.header("Visual Prediksi")
         
-        visualize_predictions_all_pred(data,train_size_open,train_size_close, n_steps, b_pred, y_pred)
+        visualize_predictions_all_pred(data, train_size_open, train_size_close, train_size_high, n_steps, b_pred, y_pred, d_pred)
         
-#Perhitungan
+#---------------------------------------#
 def prepare_data_close(data, n_steps):
     X, y = [], []
     for i in range(len(data) - n_steps):
@@ -217,6 +269,15 @@ def prepare_data_open(data, n_steps):
         A.append(np.concatenate([lag_values_open, [data[i + n_steps, 0]]]))
         b.append(data[i + n_steps, 0])
     return np.array(A), np.array(b)
+
+def prepare_data_high(data, n_steps):
+    C, d = [], []
+    for i in range(len(data) - n_steps):
+        lag_values_high = data[i:(i + n_steps), 0]
+        C.append(np.concatenate([lag_values_high, [data[i + n_steps, 0]]]))
+        d.append(data[i + n_steps, 0])
+    return np.array(C), np.array(d)
+#---------------------------------------#
 
 #Visual Grafik_Close
 def visualize_predictions_close(data, train_size_close, n_steps, y_test_orig, y_pred):
@@ -281,8 +342,39 @@ def visualize_predictions_open(data, train_size_open, n_steps, b_test_orig, b_pr
 
     st.plotly_chart(fig_open)
     
+
+#Visual Grafik_High
+def visualize_predictions_high(data, train_size_high, n_steps, d_test_orig, d_pred):
+    fig_high = go.Figure()
+
+    fig_high.add_trace(go.Scatter(x=data.index[:train_size_high + n_steps],  # Menambahkan n_steps untuk data latih
+                             y=data['High'][:train_size_high + n_steps],  # Menambahkan n_steps untuk data latih
+                             mode='lines',
+                             name="Training Data",
+                             line=dict(color='gray')))
+
+    fig_high.add_trace(go.Scatter(x=data.index[train_size_high + n_steps:],
+                             y=d_test_orig.flatten(),
+                             mode='lines',
+                             name="Actual Stock Prices",
+                             line=dict(color='blue')))
+
+    fig_high.add_trace(go.Scatter(x=data.index[train_size_high + n_steps:],
+                             y=d_pred.flatten(),
+                             mode='lines',
+                             name="Predicted Stock Prices",
+                             line=dict(color='red')))
+
+    fig_high.update_layout(title="High Price Prediction",
+                      xaxis_title="Date",
+                      yaxis_title="Stock Price (USD)",
+                      template='plotly_dark')
+        
+    st.plotly_chart(fig_high)
+
+    
 #Visual Grafik All Predict
-def visualize_predictions_all_pred(data,train_size_open,train_size_close, n_steps, b_pred, y_pred):
+def visualize_predictions_all_pred(data,train_size_open,train_size_close, train_size_high, n_steps, b_pred, y_pred, d_pred):
     fig_all_prices_predict = go.Figure()
     
     fig_all_prices_predict.add_trace(go.Scatter(x=data.index[train_size_open + n_steps:],
@@ -296,6 +388,17 @@ def visualize_predictions_all_pred(data,train_size_open,train_size_close, n_step
                              mode='lines',
                              name="Prediksi Close",
                              line=dict(color='green')))
+    
+    fig_all_prices_predict.add_trace(go.Scatter(x=data.index[train_size_high + n_steps:],
+                             y=d_pred.flatten(),
+                             mode='lines',
+                             name="Prediksi High",
+                             line=dict(color='yellow')))
+    
+    fig_all_prices_predict.update_layout(title="All Price Prediction",
+                      xaxis_title="Date",
+                      yaxis_title="Stock Price (USD)",
+                      template='plotly_dark')
     
     st.plotly_chart(fig_all_prices_predict)
 
